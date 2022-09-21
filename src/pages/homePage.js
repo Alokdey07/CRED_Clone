87c12c3bb444adb5fad349ac5e95f56c2a4ce12f
Alokdey07 <alokdeykv@gmail.com>
2022-09-21 09:29:15 +0530

    alok

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,8 +1,23 @@
 import React from "react";
+import Header from "../components/Common/Header";
+import CredExperience from "../components/CredExperience";
+import CredSecurity from "../components/CredSecurity";
+import FeelSpecial from "../components/FeelSpecial";
 import HeroSection from "../components/HeroSection";
+import ProductShowcase from "../components/ProductShowcase";
+
 
 const HomePage = () => {
-    return <HeroSection />
+    return (
+    <> 
+    <Header />
+    <HeroSection /> 
+    <ProductShowcase />
+    <FeelSpecial />
+    <CredExperience />
+    <CredSecurity />
+    </>
+    );
 };
 
 export default HomePage;
